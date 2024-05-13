3675fcb2c62175f78bd9eee18c53db72706a8e64
Aya Hany <89316483+Ayahany242@users.noreply.github.com>
2024-05-13 09:07:09 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by AYA on 13/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
